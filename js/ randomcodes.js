7f5283709ef2d39fc55026fbb48df70db7deaa77
Meishen Yin <meishenyin@Meishens-MacBook-Pro.local>
2022-05-30 13:05:43 -0700

    W4D2

--- js/ randomcodes.js
+++ js/ randomcodes.js
@@ -1,8 +1,19 @@
+
+
+
+var code="";
+var getCode="";
+var btnvalue;
+var str =" abcdefghijllmnopq";
+
+var codebox=document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
+
+codebox.addEventListener("input", evaluateCode);
+
+
+
 function generateCode(){
-
-    var code = ";"
-    var str ="abcdefghijklmn"
-
     for (i=1; i<=8;i++){
     var char = Math.random()*str.length;
     code +=str.charAt(char)
@@ -20,3 +31,34 @@ function generateCode(){
 
     disableButton();
 }
+
+function disableButton(btnvalue){
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue==true){
+        document.getElementById("submit").style.backgroundColor = "rgba(73,119,209,0.3)";
+        document.getElementById("submit").style.color = "rgba(255,255,255,0.5)";
+    }else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73,119,209,1)";
+        document.getElementById("submit").style.color = "rgba(255,255,255,1)";
+    }
+}
+
+function evaluateCode(){
+        getCode = document.getElementById("codeentered").value;
+        var charset1= getCode.trim();
+        var charset2 = code.trim();
+
+        if(charset1.length == charset2.length && charset1 == charset2)
+        {disableButton(false);}
+}
+
+
+
+
+
+
+
+
+
+
+
